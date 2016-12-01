e82a0fc80648224333579ee4c5f028d5adf731c1
Bhaskar Karambelkar <bhaskarvk@gmail.com>
2016-11-30 20:35:05 -0500

    Updated to latest GPS plugin, and added more options and methods.

--- R/gps.R
+++ R/gps.R
@@ -1,31 +1,92 @@
 leafletGPSDependencies <- function() {
   list(
     htmltools::htmlDependency(
-      "leaflet-gps",
-      "0.1.0",
-      system.file("htmlwidgets/lib/leaflet-gps", package = "leaflet.extras"),
-      script = c('leaflet-gps.min.js', 'leaflet-gps-binding.js'),
+      "gps",
+      "1.5.0",
+      system.file("htmlwidgets/lib/gps", package = "leaflet.extras"),
+      script = c('leaflet-gps.min.js', 'gps-bindings.js'),
       stylesheet = c('leaflet-gps.min.css')
     )
   )
 }
 
-#' Add a gps to the Map
-#' \url{https://github.com/Norkart/Leaflet-MiniMap}
+#' Options for the GPS Control
+#' @param position Position of the Control
+#' @param activate If TRUE activates the GPS on addition.
+#' @param autoCenter If TRUE auto centers the map when GPS location changes
+#' @param maxZoom If set zooms to this level when auto centering
+#' @param setView If TRUE sets the view to the GPS location when found
+#' @rdname gps
+#' @export
+gpsOptions <- function(
+  position = 'topleft',
+  activate = FALSE,
+  autoCenter = FALSE,
+  maxZoom = NULL,
+  setView = FALSE
+) {
+  leaflet::filterNULL(list(
+    position = position,
+    activate = activate,
+    autoCenter = autoCenter,
+    maxZoom = maxZoom,
+    setView = setView
+  ))
+}
+
+#' Add a gps to the Map.
 #'
 #' @param map a map widget object
-#' @examples
-#' leaflet() %>%
-#'   addTiles() %>%
-#'   addControlGPS()
+#' @param options Options for the GPS control.
+#' @rdname gps
 #' @export
 addControlGPS <- function(
-  map
+  map,
+  options = gpsOptions()
 ) {
   map$dependencies <- c(map$dependencies, leafletGPSDependencies())
   invokeMethod(
-    map
-    , getMapData(map)
-    , 'addControlGPS'
+    map,
+    getMapData(map),
+    'addControlGPS',
+    leaflet::filterNULL(options)
+  )
+}
+
+#' Removes the GPS Control
+#' @rdname gps
+#' @export
+removeControlGPS <- function(map) {
+  map$dependencies <- c(map$dependencies, leafletGPSDependencies())
+  invokeMethod(
+    map,
+    getMapData(map),
+    'removeControlGPS'
+  )
+}
+
+#' Activate the GPS Control.
+#' You should have already added the GPS control before calling this method.
+#' @rdname gps
+#' @export
+activateGPS <- function(map) {
+  map$dependencies <- c(map$dependencies, leafletGPSDependencies())
+  invokeMethod(
+    map,
+    getMapData(map),
+    'activateGPS'
+  )
+}
+
+#' Deactivate the GPS Control.
+#' You should have already added the GPS control before calling this method.
+#' @rdname gps
+#' @export
+deactivateGPS <- function(map) {
+  map$dependencies <- c(map$dependencies, leafletGPSDependencies())
+  invokeMethod(
+    map,
+    getMapData(map),
+    'deactivateGPS'
   )
 }
