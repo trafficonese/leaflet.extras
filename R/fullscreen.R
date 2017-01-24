d6b9a58beb2d68a63129a1ebb0f57d4017482f5a
Bhaskar Karambelkar <bhaskarvk@gmail.com>
2017-01-23 21:47:31 -0500

    Fixed #41.

--- R/fullscreen.R
+++ R/fullscreen.R
@@ -15,6 +15,7 @@ fullscreenDependency <- function() {
 #' @description Add a fullscreen control button
 #' @param map The leaflet map
 #' @param position position of control: 'topleft', 'topright', 'bottomleft', or 'bottomright'
+#' # @param pseudoFullscreen if true, fullscreen to page width and height
 #' @rdname fullscreen
 #' @export
 #' @examples
@@ -22,10 +23,12 @@ fullscreenDependency <- function() {
 #' leaflet() %>% addTiles() %>%
 #'   addFullscreenControl()
 #' }
-addFullscreenControl <- function(map, position = 'topleft') {
+addFullscreenControl <- function(
+  map, position = 'topleft', pseudoFullscreen = FALSE) {
   map$dependencies <- c(map$dependencies, fullscreenDependency())
   if (is.null(map$x$options))
     map$x$options <- list()
-  map$x$options['fullscreenControl'] = list(list(position=position))
+  map$x$options['fullscreenControl'] <-
+    list(list(position = position, pseudoFullscreen = pseudoFullscreen))
   map
 }
