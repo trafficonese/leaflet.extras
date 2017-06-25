30fd4e0b1129b3c6920d408e411ac82bfb64e86f
Bhaskar Karambelkar <bhaskarvk@gmail.com>
2017-06-25 10:49:11 -0400

    Hopefully CRAN ready now

--- R/leaflet.extras-package.r
+++ R/leaflet.extras-package.r
@@ -1,4 +1,11 @@
-#' leaflet.extras.
+#' leaflet.extras: Extra Functionality for 'leaflet' Package.
+#'
+#' Description: The 'leaflet' JavaScript library provides many plugins some of which
+#' are available in the core 'leaflet' package, but there are many more. It is not
+#' possible to support them all in the core 'leaflet' package. This package serves
+#' as an add-on to the 'leaflet' package by providing extra functionality via 'leaflet'
+#' plugins.
+#'
 #' @importFrom magrittr %>%
 #' @import leaflet
 #'
