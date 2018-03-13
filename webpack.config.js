0dcd91ecdf5aaafdab683dd3c56da0eeb0867821
Barret Schloerke <schloerke@gmail.com>
2018-03-13 16:09:06 -0400

    use napa instead of forked repos. add missing npm pkgs.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require("path");
 // const webpack = require('webpack');
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const src_path = "./inst/htmlwidgets/src/";
+const lib_path = "./inst/htmlwidgets/lib/";
 const build_path = path.resolve(__dirname, "./inst/htmlwidgets/build");
 
 library_module = function(name) {
@@ -14,7 +16,7 @@ library_module = function(name) {
           {
             loader: 'file-loader',
             options: {
-              name: name + "-[hash].[ext]"
+              name: name + "_css/" + name + "-[hash].[ext]"
             }
           }
         ]
@@ -29,12 +31,13 @@ library_module = function(name) {
 
 
 library_prod = function(name, filename = name, library = undefined) {
-  filename = filename + "_prod"
+  foldername = filename
+  filename = filename + "-prod"
   var ret = {
     mode: "production",
     entry: name,
     // devtool: "source-map",
-    module: library_module(filename),
+    module: library_module(foldername),
     output: {
       // library: library,
       filename: filename + ".js",
@@ -51,40 +54,93 @@ library_prod_externals = function(externals, ...info) {
   ret.externals = externals;
   return ret;
 }
+library_prod_attachements = function(attachments, output_folder, ...info) {
+  var ret = library_prod(...info)
+  ret.plugins = [
+    new CopyWebpackPlugin(
+      [{
+        from: attachments,
+        to: build_path + "/" + output_folder,
+        flatten: true
+      }]
+    )
+  ]
+  return ret;
+}
 
-library_binding = function(name) {
-  filename = name + "-bindings"
+library_raw = function(entry, filename, foldername) {
   return {
-    entry: src_path + filename + ".js",
+    entry: entry,
     output: {
-      filename: filename + ".js",
-      path: build_path
+      filename: filename,
+      path: build_path + "/" + foldername
     },
-    module: library_module(filename),
-    // devtool: "source-map",
-    devtool: "inline-source-map",
-    mode: "development"
+    module: {
+      rules: [
+        {use: "raw-loader"}
+      ]
+    }
   }
+
+  // return {
+  //   entry: entry,
+  //   output: {
+  //     filename: filename,
+  //     path: build_path + "/" + foldername
+  //   },
+  //   module: library_module(filename),
+  //   // devtool: "source-map",
+  //   devtool: "inline-source-map",
+  //   mode: "development"
+  // }
+}
+library_binding = function(name) {
+  var filename = name + "-bindings"
+  var folder = "bindings"
+  return library_raw(
+    src_path + folder + "/" + filename + ".js",
+    filename + ".js",
+    name
+  )
 }
 
 const config = [
 
   // library_prod(LIBRARY, SAVE_NAME, GLOBAL_JS_VAR_NAME)
 
-  // "@mapbox/leaflet-omnivore": "0.3.4",
+  // library_raw(
+  //   src_path + "utils/leaflet_extras-utils.js",
+  //   "leaflet_extras-utils.js",
+  //   "leaflet_extras-utils"
+  // ),
+
+
+  // "csv2geojson": "5.0.2",
+  // "togeojson": "0.16.0",
+  // "topojson": "3.0.2"
   library_prod("csv2geojson", "csv2geojson", "csv2geojson"),
-  library_prod("togeojson", "toGeoJSON", "toGeoJSON"),
+  library_prod("togeojson", "togeojson", "toGeoJSON"),
   library_prod("topojson", "topojson", "topojson"),
+
+  // "@mapbox/leaflet-omnivore": "0.3.4",
   library_prod_externals({
     topojson: "topojson",
     csv2geojson: "csv2geojson",
     togeojson: "togeojson",
   }, "@mapbox/leaflet-omnivore", "leaflet-omnivore"),
-  library_binding("omnivore"),
+  // library_binding("leaflet-omnivore"),
 
   // "Leaflet.Geodesic": "github:henrythasler/Leaflet.Geodesic#c5fe36b",
   library_prod("Leaflet.Geodesic", "leaflet-geodesic"),
-  library_binding("geodesic"),
+  // library_binding("leaflet-geodesic"),
+
+  // "Leaflet.StyleEditor": "github:dwilhelm89/Leaflet.StyleEditor#24366b9"
+  library_prod(
+    ["Leaflet.StyleEditor", "Leaflet.StyleEditor/dist/css/Leaflet.StyleEditor.min.css"],
+    "leaflet-styleeditor"
+  ),
+  // library_binding("leaflet-styleeditor"),
+
 
   // "leaflet-choropleth": "1.1.4",
   library_prod("leaflet-choropleth"),
@@ -96,7 +152,7 @@ const config = [
     "leaflet-draw"
   ),
   library_prod("leaflet-draw-drag"),
-  library_binding("draw"),
+  // library_binding("leaflet-draw"),
 
   // "leaflet-fullscreen": "1.0.2",
   library_prod(
@@ -111,17 +167,18 @@ const config = [
   ),
 
   // "leaflet-hash": "github:PowerPan/leaflet-hash#4020d13",
-  library_prod("leaflet-hash/dist/leaflet-hash.min.js"),
+  library_prod("leaflet-hash/dist/leaflet-hash.min.js", "leaflet-hash"),
 
   // "leaflet-measure-path": "1.3.1",
   library_prod(
     ["leaflet-measure-path", "leaflet-measure-path/leaflet-measure-path.css"],
     "leaflet-measure-path"
   ),
-  library_binding("measure-path"),
+  // library_binding("leaflet-measure-path"),
 
   // "leaflet-plugins": "3.0.2",
   library_prod("leaflet-plugins/layer/tile/Bing.js", "tile-bing"),
+  // library_binding("tile-bing"),
     // For google support!!
     // "leaflet.gridlayer.googlemutant": "^0.6.4",
 
@@ -130,7 +187,7 @@ const config = [
     ["leaflet-pulse-icon", "leaflet-pulse-icon/src/L.Icon.Pulse.css"],
     "leaflet-pulse-icon"
   ),
-  library_binding("pulse-icon"),
+  // library_binding("leaflet-pulse-icon"),
 
   // "fuse.js": "3.2.0",
   // "leaflet-search": "2.3.7",
@@ -139,44 +196,48 @@ const config = [
     ["leaflet-search", "leaflet-search/dist/leaflet-search.min.css"],
     "leaflet-search"
   ),
+  // library_binding("leaflet-search"),
 
   // "leaflet-sleep": "0.5.1",
   library_prod("leaflet-sleep"),
 
   // "leaflet-webgl-heatmap": "0.2.7",
-  library_prod(
+  library_prod_attachements(
+    "node_modules/leaflet-webgl-heatmap/src/webgl-heatmap/*.png",
+    "leaflet-webgl-heatmap",
     ["leaflet-webgl-heatmap", "leaflet-webgl-heatmap/src/leaflet-webgl-heatmap.js"],
     "leaflet-webgl-heatmap"
   ),
-  library_binding("webgl-heatmap"),
+  // library_binding("leaflet-webgl-heatmap"),
 
-  // "leaflet-wms-legend": "github:schloerke/leaflet-wms-legend#c97fde8",
+  // napa kartoza/leaflet-wms-legend#0f59578:leaflet-wms-legend
   library_prod(
-    ["leaflet-wms-legend", "leaflet-wms-legend/leaflet.wmslegend.css"],
+    ["leaflet-wms-legend/leaflet.wmslegend.js", "leaflet-wms-legend/leaflet.wmslegend.css"],
     "leaflet-wms-legend"
   ),
 
 
   // "leaflet.heat": "0.2.0",
   // TODO!!! use distributed version from somewhere
-  library_prod(src_path + "leaflet-heat.js", "leaflet-heat"),
-  library_binding("heat"),
+  library_prod(lib_path + "heat/leaflet-heat.js", "leaflet-heat"),
+  // library_binding("leaflet-heat"),
 
+  // "pouchdb": "6.4.3",
   // "leaflet.tilelayer.pouchdbcached": "0.3.0",
   library_prod("pouchdb", "pouchdb", "PouchDB"),
-  library_prod("leaflet.tilelayer.pouchdbcached"),
+  library_prod("leaflet.tilelayer.pouchdbcached", "leaflet-tilelayer-pouchdbcached"),
 
-  // "leaflet.weather-markers": "github:schloerke/Leaflet.weather-markers#a4389f1",
+  // napa tallsam/Leaflet.weather-markers#afda5b3:leaflet-weather-markers
   library_prod(
     [
-      "leaflet.weather-markers",
-      "leaflet.weather-markers/dist/leaflet.weather-markers.css",
-      src_path + "weather-icons/weather-icons.min.css",
-      src_path + "weather-icons/weather-icons-wind.min.css"
+      "leaflet-weather-markers/dist/leaflet.weather-markers.js",
+      "leaflet-weather-markers/dist/leaflet.weather-markers.css",
+      lib_path + "weather-icons/weather-icons.min.css",
+      lib_path + "weather-icons/weather-icons-wind.min.css"
     ],
-    "leaflet.weather-markers"
+    "leaflet-weather-markers"
   ),
-  library_binding("weather-markers")
+  // library_binding("leaflet-weather-markers")
 
 
 
