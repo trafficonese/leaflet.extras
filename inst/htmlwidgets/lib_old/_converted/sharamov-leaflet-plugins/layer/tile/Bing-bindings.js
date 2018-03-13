b22c4e4e00358ebe6899e1046fd1fc1225163cb0
Barret Schloerke <schloerke@gmail.com>
2018-03-13 16:02:16 -0400

    move all old libraries to lib_old

--- inst/htmlwidgets/lib_old/_converted/sharamov-leaflet-plugins/layer/tile/Bing-bindings.js
+++ inst/htmlwidgets/lib_old/_converted/sharamov-leaflet-plugins/layer/tile/Bing-bindings.js
@@ -4,10 +4,9 @@ LeafletWidget.methods.addBingTiles = function(layerId, group, options) {
   (function(){
     var map = this;
     var apikey = options.apikey;
-    delete options.apikey;
+      delete options.apikey;
 
     map.layerManager.addLayer(L.bingLayer(apikey, options), "tile", layerId, group);
 
   }).call(this);
 };
-
