8f6a4d1fe47848e493c2c15a6ff97cf2713d6b4c
Barret Schloerke <schloerke@gmail.com>
2018-03-21 14:47:37 -0700

    move bounce param up and document

--- R/bounceMarker.R
+++ R/bounceMarker.R
@@ -17,9 +17,9 @@ bounceMarkerDependency <- function() {
 }
 
 #' Add Bounce Markers to map
+#' @param map map object created by [leaflet::leaflet]
 #' @param lat numeric latitude
 #' @param lng numeric longitude
-#' @param map map object created by [leaflet::leaflet]
 #' @param duration integer scalar: The duration of the animation in milliseconds.
 #' @param height integer scalar: Height at which the marker is dropped.
 #' @md
@@ -27,9 +27,9 @@ bounceMarkerDependency <- function() {
 #' @export
 #' @seealso [Github: leaflet.bouncemarker](https://github.com/maximeh/leaflet.bouncemarker)
 #' @examples
-#' \dontrun{
-#' leaflet() %>% addTiles() %>% addBounceMarkers(49, 11)
-#' }
+#' leaflet() %>%
+#'   addTiles() %>%
+#'   addBounceMarkers(49, 11)
 addBounceMarkers = function(map, lat, lng, duration = 1000, height = 100) {
   map$dependencies <- c(map$dependencies, bounceMarkerDependency())
   invokeMethod(map, getMapData(map), 'addBounceMarkers', lat, lng, duration, height)
