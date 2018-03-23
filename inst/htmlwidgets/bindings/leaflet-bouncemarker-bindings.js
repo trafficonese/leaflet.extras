5452cdd9bb655bb10669f130f6927ce48f255472
Barret Schloerke <schloerke@gmail.com>
2018-03-23 17:51:42 -0400

    spacing and remove console.log

--- inst/htmlwidgets/bindings/leaflet-bouncemarker-bindings.js
+++ inst/htmlwidgets/bindings/leaflet-bouncemarker-bindings.js
@@ -1,12 +1,18 @@
 /* global LeafletWidget, $, L */
 
 LeafletWidget.methods.addBounceMarkers = function(lat, lng, duration, height) {
-  (function() {
-   L.marker([lat, lng],
-  {
-    bounceOnAdd: true,
-    bounceOnAddOptions: {duration: duration, height: height},
-    bounceOnAddCallback: function() {console.log("done");}
-  }).addTo(this);
-  }).call(this);
+  (
+    function() {
+      L.marker(
+        [lat, lng],
+        {
+          bounceOnAdd: true,
+          bounceOnAddOptions: {
+            duration: duration,
+            height: height
+          }
+        }
+      ).addTo(this);
+    }
+  ).call(this);
 };
