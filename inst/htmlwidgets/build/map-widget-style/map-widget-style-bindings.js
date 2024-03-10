53ebfde83fdb8822f0448a348d610b9306e79af7
Sebastian Gatscha <gatscha@trafficon.eu>
2024-03-10 18:14:48 +0100

    build not minified

--- inst/htmlwidgets/build/map-widget-style/map-widget-style-bindings.js
+++ inst/htmlwidgets/build/map-widget-style/map-widget-style-bindings.js
@@ -1,20 +1,2 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!****************************************************************!*\
-  !*** ./inst/htmlwidgets/bindings/map-widget-style-bindings.js ***!
-  \****************************************************************/
-/* global LeafletWidget, $ */
-
-LeafletWidget.methods.setMapWidgetStyle = function(style) {
-  var widget = this;
-  if($.isEmptyObject(widget._container.style)) {
-    widget._container.style = {};
-  }
-  $.each(style, function(key, value) {
-    widget._container.style[key] = value;
-  });
-};
-
-/******/ })()
-;
+LeafletWidget.methods.setMapWidgetStyle=function(t){var e=this;$.isEmptyObject(e._container.style)&&(e._container.style={}),$.each(t,(function(t,n){e._container.style[t]=n}))};
 //# sourceMappingURL=map-widget-style-bindings.js.map
