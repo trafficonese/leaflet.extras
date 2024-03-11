fee1515542baa40dce8f28ebf96bad488bb2afb8
Sebastian Gatscha <gatscha@trafficon.eu>
2024-03-12 00:52:54 +0100

    builds

--- inst/htmlwidgets/build/lfx-bouncemarker/lfx-bouncemarker-bindings.js
+++ inst/htmlwidgets/build/lfx-bouncemarker/lfx-bouncemarker-bindings.js
@@ -1,27 +1,2 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!****************************************************************!*\
-  !*** ./inst/htmlwidgets/bindings/lfx-bouncemarker-bindings.js ***!
-  \****************************************************************/
-/* global LeafletWidget, L */
-
-LeafletWidget.methods.addBounceMarkers = function(lat, lng, duration, height) {
-  (
-    function() {
-      L.marker(
-        [lat, lng],
-        {
-          bounceOnAdd: true,
-          bounceOnAddOptions: {
-            duration: duration,
-            height: height
-          }
-        }
-      ).addTo(this);
-    }
-  ).call(this);
-};
-
-/******/ })()
-;
+LeafletWidget.methods.addBounceMarkers=function(d,n,e,t){(function(){L.marker([d,n],{bounceOnAdd:!0,bounceOnAddOptions:{duration:e,height:t}}).addTo(this)}).call(this)};
 //# sourceMappingURL=lfx-bouncemarker-bindings.js.map
